43e28491553b6c292513b1ceca6ea0bc08c49834
Darkthunder119 <gurtajchhabra@gmail.com>
2020-06-17 02:42:23 -0400

    added new reusable components

---
+++ capstone-app/src/components/TopMap/TopMap.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default class TopMap extends React.Component{
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return(<></>)
+    }
+}
